1c9677284aeeffa154e4ff1b277f7787d62ff224
SSpvse <sspasenic@gmail.com>
2025-08-24 18:04:17 +0200

    first commit

---
+++ DailyRoutine/src/test/java/com/SS/DailyRoutine/DailyRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SS.DailyRoutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
